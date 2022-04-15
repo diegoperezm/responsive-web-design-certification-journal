62b2927135583727d999e541b7bf3ce1ed334cc5
erretres <diegoperezcorreo@gmail.com>
2022-04-15 09:23:29 -0400

    Destructuring props: Component Graph.
    From data to dataYear in layout.
    From props to dataYear graph/index.js .

--- src/components/graph/index.js
+++ src/components/graph/index.js
@@ -1,37 +1,39 @@
-import   React           from 'react';
-import { Bar, defaults } from 'react-chartjs-2';
-import { dataChartjs   } from '../../consts/';
-import { getDataGraph  } from '../../helpers/';
+import React from "react";
+import { Bar, defaults } from "react-chartjs-2";
+import { dataChartjs } from "../../consts/";
+import { getDataGraph } from "../../helpers/";
 
 defaults.global.animation = false;
-defaults.global.maintainAspectRatio = false
+defaults.global.maintainAspectRatio = false;
 
-export default function Graph(props) {
-    let dataYear = props.data;
-    let data     = getDataGraph(dataChartjs, dataYear ); 
+export default function Graph({ dataYear }) {
+  let data = getDataGraph(dataChartjs, dataYear);
 
-    return (
+  return (
     <div className="mb-5 canvas-container">
-        <Bar
-          data={data}
-            options={{
-                scales: {
-                    yAxes: [{
-                        stacked: true,
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }],
-                    xAxes: [{
-                        stacked: true,
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                    
-                }
-            }}
-        />
+      <Bar
+        data={data}
+        options={{
+          scales: {
+            yAxes: [
+              {
+                stacked: true,
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                stacked: true,
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        }}
+      />
     </div>
-    );
-};
+  );
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,128 +1,123 @@
 /* Abbrev:
- * responsive web design certification =>  rwdc-2021 
+ * responsive web design certification =>  rwdc-2021
  */
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../index.css";
 
-import   React    from 'react';
-import { useEffect, useState,  useCallback }   from 'react';
-import   produce  from 'immer';
+import React from "react";
+import { useEffect, useState, useCallback } from "react";
+import produce from "immer";
 
-import {
-    Container,
-    Col,
-    Row
-}                                from 'react-bootstrap';
+import { Container, Col, Row } from "react-bootstrap";
 
-import {
-    HashRouter as Router,
-    Switch,
-    Route
-}                                from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import { rwdc2021, currentMonthName, currentDayNumber } from "../consts/";
 
 import {
-    rwdc2021,
-    currentMonthName,
-    currentDayNumber,
-}                                 from '../consts/';
+  getInitialData,
+  getMonthNumberFromName,
+  getTotalHrsMinutes,
+  getDayTotalTime,
+  getMonthTotalTime,
+} from "../helpers/";
 
-
-import {
-    getInitialData,
-    getMonthNumberFromName,
-    getTotalHrsMinutes,
-    getDayTotalTime,
-    getMonthTotalTime
-}                                 from '../helpers/';
-
-import Header                     from '../components/header/';
-import Today                      from '../components/today/';
-import Day                        from '../components/day/';
-import Year                       from '../components/year/';
-import Month                      from '../components/months/';
-import Graph                      from '../components/graph/';
+import Header from "../components/header/";
+import Today from "../components/today/";
+import Day from "../components/day/";
+import Year from "../components/year/";
+import Month from "../components/months/";
+import Graph from "../components/graph/";
 
 //import { data as DEMODATA }       from '../data/';
 
 export default function App() {
+  const currentMonthNumber = getMonthNumberFromName(currentMonthName);
+  const [data, setData] = useState(() => {
+    const initialState = getInitialData(rwdc2021, currentMonthNumber);
+    return initialState;
+  });
 
-   const currentMonthNumber  =  getMonthNumberFromName(currentMonthName);
-   const [data, setData]     = useState(() => {
-       const initialState    = getInitialData(rwdc2021, currentMonthNumber);
-       return initialState;
-    });
+  useEffect(() => {
+    localStorage.setItem("rwdc2021", JSON.stringify(data));
+  }, [data]);
 
-    
-   useEffect(() => {
-      localStorage.setItem('rwdc2021', JSON.stringify(data))
-   }, [data]); 
+  const handleChange = useCallback(
+    (evt) => {
+      const value = evt.target.value;
+      const name = evt.target.name;
+      const currentMonthDays = data.year[currentMonthNumber].days;
+      const todayTotal = getTotalHrsMinutes(data.today, name, value);
+      const dayTotalTime = getDayTotalTime(data.today, name, value);
+      const monthTotalTime = getMonthTotalTime(
+        currentMonthDays,
+        currentDayNumber,
+        dayTotalTime
+      );
 
-    const handleChange = useCallback( evt => {
-        const value             = evt.target.value
-        const name              = evt.target.name;
-        const currentMonthDays  = data.year[currentMonthNumber].days;
-        const todayTotal        = getTotalHrsMinutes(data.today, name, value);
-        const dayTotalTime      = getDayTotalTime(data.today, name, value);  
-        const monthTotalTime    = getMonthTotalTime(currentMonthDays, currentDayNumber, dayTotalTime);
+      let currentDayIndex = data.year[currentMonthNumber].days
+        .map((day) => day.dayNumber)
+        .indexOf(parseInt(currentDayNumber));
 
-        let currentDayIndex = data.year[currentMonthNumber]
-                                 .days.map(day=>day.dayNumber)
-                                 .indexOf(parseInt(currentDayNumber));
+      setData(
+        produce((draft) => {
+          draft.day[currentMonthName][currentDayNumber][evt.target.name] =
+            value;
+          draft.day[currentMonthName][currentDayNumber].totalHrs = {
+            hours: todayTotal.hours,
+            minutes: todayTotal.minutes,
+          };
 
-        setData(produce((draft) => {
+          draft.today[evt.target.name] = value;
+          draft.today.totalHrs = {
+            hours: todayTotal.hours,
+            minutes: todayTotal.minutes,
+          };
 
-            draft.day[currentMonthName][currentDayNumber][evt.target.name] = value;
-            draft.day[currentMonthName][currentDayNumber].totalHrs =  {
-                hours: todayTotal.hours,
-                minutes: todayTotal.minutes
-            }
-
-            draft.today[evt.target.name] = value;
-            draft.today.totalHrs =  {
-                hours: todayTotal.hours,
-                minutes: todayTotal.minutes
-            }
-
-            draft.year[currentMonthNumber].days[currentDayIndex].totalTime = dayTotalTime;
-            draft.year[currentMonthNumber].totalTime = monthTotalTime;
-        }))
-
-    }, [data, currentMonthNumber]);
+          draft.year[currentMonthNumber].days[currentDayIndex].totalTime =
+            dayTotalTime;
+          draft.year[currentMonthNumber].totalTime = monthTotalTime;
+        })
+      );
+    },
+    [data, currentMonthNumber]
+  );
 
   return (
-      <div className="App">
-        <Router>
-          <Header />
-            <Container>
-                <h1 className="mt-5 mb-5 title"><u>Responsive Web Design Certification:</u></h1> 
-             <Row>
-                <Col>
-             <Switch>
-               <Route exact path="/">
+    <div className="App">
+      <Router>
+        <Header />
+        <Container>
+          <h1 className="mt-5 mb-5 title">
+            <u>Responsive Web Design Certification:</u>
+          </h1>
+          <Row>
+            <Col>
+              <Switch>
+                <Route exact path="/">
                   <Today data={data.today} handleChange={handleChange} />
-               </Route>
+                </Route>
                 <Route exact path="/today">
                   <Today data={data.today} handleChange={handleChange} />
-               </Route>
-               <Route exact path={`/${currentMonthName}`}>
-                  <Month dataMonth={data.year[currentMonthNumber] } />
-               </Route>
+                </Route>
+                <Route exact path={`/${currentMonthName}`}>
+                  <Month dataMonth={data.year[currentMonthNumber]} />
+                </Route>
                 <Route exact path="/year">
-                  <Year data={data.year}  />
-               </Route>
-               <Route path="/graph">
-                 <Graph  data={data.year}/>
-               </Route>
-               <Route path="/day/">
+                  <Year data={data.year} />
+                </Route>
+                <Route path="/graph">
+                  <Graph dataYear={data.year} />
+                </Route>
+                <Route path="/day/">
                   <Day dataDay={data.day} />
-               </Route>
-             </Switch>
-             </Col>
-           </Row>
-          </Container>
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
       </Router>
     </div>
   );
 }
-
